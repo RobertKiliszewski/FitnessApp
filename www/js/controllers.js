cdd78d19f2438e802161093d35c1fa18688a0baa
RobertKiliszewski <robert.kiliszewski@hotmail.com>
2016-08-19 20:57:05 +0100

    Third Final commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,30 @@
 angular.module('starter.controllers', [])
-
+//Use $interval documentation to update calories, reference: Lab timer app.
 .controller('oneCtrl', function ($scope, Food, $interval) {
         
         $scope.data = Food.data;
     
         //delete function
         $scope.deleteFood = function (del) {
-            Food.deleteFood(del);
-        }
+        Food.deleteFood(del);
+        };
         
+        //This function counts all the calories
         function totalCalories(){
             $scope.totalCalories = Food.totalCal(); 
             console.log("TotalCal = " + $scope.totalCalories)
         }
     
+        //Scopes
         $scope.totalCalories = 0;
         $scope.foods = Food.data.foods;
         $scope.totalCal = Food.data.totalCal;
-    
+        
+        //interval keeps counting every few seconds to add up the total amount of calories  
         $interval(totalCalories, 1000);   
 })
 
-//twoCtrl
+//Controller for 
 .controller('twoCtrl', function($scope, Food, $interval){
     
     var data = {
@@ -30,26 +33,28 @@ angular.module('starter.controllers', [])
         
   }
     
-    
+    //Function for adding food and calories into the first tab
     function add(m, n){
         Food.addEntry(m, n);
         $scope.data.mytitle = "";
         $scope.data.calories = null;
     }
     
+    //This function
     function totalCalories(){
         $scope.totalCalories = Food.totalCal(); 
-        //console.log("TotalCal = " + $scope.totalCalories)
+        
     }
     
     
-  //Scopes  
-  $scope.totalCalories = 0;
-  $scope.data = data;
-  $scope.add = add;
-  $scope.totalCal = Food.data.totalCal;
+    //Scopes  
+    $scope.totalCalories = 0;
+    $scope.data = data;
+    $scope.add = add;
+    $scope.totalCal = Food.data.totalCal;
     
-  $interval(totalCalories, 1000);   
+    //This interval makes the timer for the totalCalories Loop
+    $interval(totalCalories, 1000);   
 });
 
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,36 +1,38 @@
 angular.module('starter.services', [])
 
-// Factory called FOOD
+// Factory called Food
 .factory('Food', function() {
     
+    //A variable called data that stores Food name and its calorie amount
     var data = {
-        foods: [{ mytitle: "Beans", calories: 200}],
+        foods: [{date:new Date(), mytitle: "Beans", calories: 200}],
         totalCal : 0
        
     }
 
-    
+    //This function is for calculating calories
     function calculateCal(){
         data.totalCal = 0;
-        //console.log("Running");
+        
+        //For loop for the actual calculation
         for (var i = 0; i < data.foods.length; i++)
             {
                 data.totalCal += data.foods[i].calories;
             }
-        //console.log(data.totalCal);
-        
+        //This returns the total calorie number 
         return data.totalCal;    
-        
     }
     
+    //This function adds Entries to the first tab
     function addEntry(m, n){
-        data.foods.push({mytitle: m, calories: n});
+        data.foods.push({date: new Date(), mytitle: m, calories: n});
     }
     
-    
+    //This Returns everything thats used on screen
     return {
         data: data,
         addEntry : addEntry,
+        //Delete function using splice from http://stackoverflow.com/questions/36687245/how-to-use-arrays-splice-method-in-ion-list
         deleteFood: function (del){
           data.foods.splice(data.foods.indexOf(del), 1);
       },
